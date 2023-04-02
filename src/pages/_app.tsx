0fc4b862abeaff814bb64dfe62695ef11f694dbd
Ethan Hill <ethan.hill@drummondcentral.co.uk>
2023-04-02 16:37:17 +0100

    Added document for css link, added ngprogress for SSR progress indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,47 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import Router from "next/router";
+
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 
+// Types import
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+
+// Google fonts
 import { Be_Vietnam_Pro } from "next/font/google";
 const BeVietnamPro = Be_Vietnam_Pro({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
+// CSS imports
 import "~/styles/globals.css";
+
+// Components
 import Transition from "~/components/misc/Transition";
+import Nprogress from "nprogress";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      Nprogress.start();
+    });
+
+    Router.events.on("routeChangeComplete", (url) => {
+      Nprogress.done(false);
+    });
+  }, [Router]);
+
   return (
     <SessionProvider session={session}>
       <Transition>
         <main className={BeVietnamPro.className}>
           <Component {...pageProps} />
         </main>
-      </Transition>{" "}
+      </Transition>
     </SessionProvider>
   );
 };
